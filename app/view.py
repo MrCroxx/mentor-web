fb1381c66e20de6b0dc4a8b441b01eaa68499350
Croxx <croxxpwn@gmail.com>
2017-10-17 23:15:42 +0800

    Add CAS-LOGIN.

--- app/view.py
+++ app/view.py
@@ -53,7 +53,8 @@ def index():
 @login_required
 def logout_lm():
     logout_user()
-    return redirect(url_for('index'))
+    # return redirect(url_for('index'))
+    return redirect(url_for('logout'))
 
 
 '''
